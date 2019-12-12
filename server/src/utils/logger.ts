168d0bd30ddb7c52cb9dec5ccc8afb09fc690048
NicolasNewman <newma102@gmail.com>
2019-12-12 13:56:50 -0500

    updating and changed styling plugins from boilerplate

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -2,12 +2,10 @@
  * The Logger.
  * Prints logs (errors only) on console.
  */
-import * as winston from "winston";
+import * as winston from 'winston';
 
 export const logger = winston.createLogger({
-  format: winston.format.simple(),
-  level: "error",
-  transports: [
-    new (winston.transports.Console)(),
-  ]
+    format: winston.format.simple(),
+    level: 'error',
+    transports: [new winston.transports.Console()]
 });
