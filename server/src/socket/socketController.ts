7431da3bf6d75b9f5dc76d386ffef299575c8eff
NicolasNewman <nanewman01@gmail.com>
2019-12-22 12:50:31 -0600

    removes user from list on disconnect

--- server/src/socket/socketController.ts
+++ server/src/socket/socketController.ts
@@ -1,6 +1,7 @@
 import { Application } from 'express';
 import * as http from 'http';
 import * as socketIO from 'socket.io';
+import { logger } from '../utils/logger';
 
 export const socketEvents = {
     registerUser: 'registerUser'
@@ -10,22 +11,36 @@ export const emitableEvents = {
     isAdmin: 'isAdmin'
 };
 
+interface IIDtoUserMap {
+    [key: string]: string;
+}
+
 export default class SocketController {
     app: Application;
     io: socketIO.Server;
     httpServer: http.Server;
     registeredUsers: Array<string>;
+    idToUserMap: IIDtoUserMap;
 
     constructor(app: Application) {
         this.app = app;
         this.registeredUsers = [];
+        this.idToUserMap = {};
 
         this.httpServer = http.createServer(this.app);
         this.io = socketIO(this.httpServer);
         this.io.on('connection', socket => {
-            socket.on('disconnect', function() {
-                //TODO remove user
-                console.log('A user disconnected!');
+            logger.log('info', `User ${socket.id} connected`);
+            socket.on('disconnect', () => {
+                const name = this.idToUserMap[socket.id];
+                this.registeredUsers = this.registeredUsers.filter(
+                    e => e !== name
+                );
+                logger.log(
+                    'info',
+                    `User ${socket.id} disconnected. Removing ${name} form the user list`
+                );
+                console.log();
             });
             socket.on(
                 socketEvents.registerUser,
@@ -36,6 +51,11 @@ export default class SocketController {
                         nameTakenCallback(true);
                     } else {
                         this.registeredUsers.push(name);
+                        this.idToUserMap[socket.id] = name;
+                        logger.log(
+                            'info',
+                            `User ${socket.id} registered as ${name}`
+                        );
                         if (name === process.env.PASSWORD) {
                             socket.emit(emitableEvents.isAdmin, true);
                             nameTakenCallback(false);
