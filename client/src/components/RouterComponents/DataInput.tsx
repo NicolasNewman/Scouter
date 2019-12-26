960cbdb07fcecf5b01d0661bbba4a9b4f2ed105c
NicolasNewman <nanewman01@gmail.com>
2019-12-26 12:03:13 -0600

    added less component for scouting form

--- client/src/components/RouterComponents/DataInput.tsx
+++ client/src/components/RouterComponents/DataInput.tsx
@@ -17,18 +17,41 @@ export default class Home extends Component<IProps> {
   }
 
   render() {
-    return (
-      <div>
-        <h1>You are scouting: {this.props.scoutingTargets}</h1>
-        <h2>For match {this.props.matchNumber}</h2>
-        <Tabs>
-          {this.props.scoutingTargets.map(team => (
-            <TabPane tab={team} key={team}>
-              <h1>Insert form here</h1>
-            </TabPane>
-          ))}
-        </Tabs>
-      </div>
-    );
+    if (this.props.scoutingTargets && this.props.matchNumber) {
+      return (
+        <div>
+          <h1>Match: {this.props.matchNumber}</h1>
+          <h1>Scouting: {this.props.scoutingTargets.join(", ")}</h1>
+          <Tabs>
+            {this.props.scoutingTargets.map(team => (
+              <TabPane tab={team} key={team}>
+                <h1>Insert form here</h1>
+              </TabPane>
+            ))}
+          </Tabs>
+        </div>
+      );
+    } else {
+      // return (
+      //   <div>
+      //     <h1>The scouting form isn't ready yet.</h1>
+      //   </div>
+      // );
+      const matchNumber = 1;
+      const scoutingTargets = [100, 200, 300];
+      return (
+        <div className="scouting">
+          <h1>Match: {matchNumber}</h1>
+          <h1>Scouting: {scoutingTargets.join(", ")}</h1>
+          <Tabs>
+            {scoutingTargets.map(team => (
+              <TabPane tab={team} key={team + ""}>
+                <h1>Insert form here</h1>
+              </TabPane>
+            ))}
+          </Tabs>
+        </div>
+      );
+    }
   }
 }
