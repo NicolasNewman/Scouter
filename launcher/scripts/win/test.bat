f3803f0ce1da12fdc3c0653b60cf0b457fbb26bd
NicolasNewman <nanewman01@gmail.com>
2020-01-02 12:24:06 -0600

    added shell scripts for installing, building, and running the server

---
+++ launcher/scripts/win/test.bat
@@ -0,0 +1,11 @@
+:: Final file structure:
+:: resources/
+:: ├── server/
+:: │   └── package.json
+:: ├── client/
+:: │   └── package.json
+:: ├── scripts/
+:: │   ├── win/
+:: │   │   └── [you are here]
+:: │   └── unix/
+:: └── package.json
