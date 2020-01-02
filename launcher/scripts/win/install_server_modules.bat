bc1be3cc9bc67358dd895f5efee8ac1c301bf8a3
NicolasNewman <nanewman01@gmail.com>
2020-01-02 17:06:55 -0600

    added Windows bat scripts

---
+++ launcher/scripts/win/install_server_modules.bat
@@ -0,0 +1,14 @@
+:: Final file structure:
+:: resources/
+:: ├── server/
+:: │   └── package.json
+:: ├── client/
+:: │   └── package.json
+:: ├── scripts/
+:: │   ├── win/
+:: │   └── unix/
+:: │       └── [you are here]
+:: └── package.json
+
+cd ../../server
+yarn install
