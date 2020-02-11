4da212f0b17df4d7fbdb383b747ee1b8f28b0abc
NicolasNewman <nanewman01@gmail.com>
2020-02-11 12:10:01 -0500

    dataCompiler now gathers more data

--- server/src/utils/dataCompiler.ts
+++ server/src/utils/dataCompiler.ts
@@ -4,7 +4,7 @@ import {
     EScorableRobotEvents,
     IRobotEvent,
     Phase,
-    IRobotState,
+    // IRobotState,
     gameProperties
 } from '../global/gameTypes';
 
@@ -12,29 +12,32 @@ const dataCompiler = async (): Promise<string> => {
     const res = await fetch('http://localhost:3000/data/matches');
     const json = await res.json();
     const matches: IVritualizedMatches = json.data.matches;
-    let data = '';
+    let allData = '';
     matches.forEach(match => {
+        let data = '';
         data = addDataToRow(data, match.matchNumber);
+        data = addDataToRow(data, 'color');
         data = addDataToRow(data, match.teamNumber);
         // Initiation?
         data = addDataToRow(
             data,
             eventHappened(match.robotEvents, EScorableRobotEvents.INITIATION)
         );
-
+        data = addDataToRow(data, 'played in auto');
         // Get auto scores
-        let locations: Array<string> = [];
-        let attempt = 0;
-        let scored = 0;
         let autoCycleEnd = 0;
         match.cycle.forEach(cycle => {
+            // TODO IGNORE OVER 3
             if (cycle.interval.start) {
-                if (getPhaseFromStartTime(cycle.interval.start) === "AUTO") {
+                if (getPhaseFromStartTime(cycle.interval.start) === 'AUTO') {
                     autoCycleEnd++;
-                    const primaryEvent = getPrimaryCycleEvent(cycle.cycleEvents);
-                    if (!locations.includes(primaryEvent)) {
-                        locations.push(primaryEvent);
-                    }
+                    const primaryEvent = getPrimaryCycleEvent(
+                        cycle.cycleEvents
+                    );
+                    // Where?
+                    data = addDataToRow(data, primaryEvent);
+                    let attempt = 0;
+                    let scored = 0;
                     cycle.cycleEvents.forEach(event => {
                         if (event.type === primaryEvent) {
                             if (event.success) {
@@ -43,40 +46,75 @@ const dataCompiler = async (): Promise<string> => {
                             }
                         }
                     });
+                    data = addDataToRow(data, attempt);
+                    data = addDataToRow(data, scored);
                 }
             }
         });
-        data = addDataToRow(data, locations.join(', '));
-        data = addDataToRow(data, attempt);
-        data = addDataToRow(data, scored);
+        // Fill in the empty cycles
+        if (autoCycleEnd < 3) {
+            for (let i = autoCycleEnd; i < 3; i++) {
+                data = addDataToRow(data, '');
+                data = addDataToRow(data, '');
+                data = addDataToRow(data, '');
+            }
+        }
 
-        let cyclesWritten = 0;
-        let cycleMax = ;
+        // Fill the remaining auto cycles
+        if (autoCycleEnd < 3) {
+            data = addEmptyColumns(data, 3 - autoCycleEnd);
+        }
+
         match.cycle.forEach((cycle, i) => {
-            if (i >= autoCycleEnd) {
-
+            if (i > autoCycleEnd) {
+                const primaryEvent = getPrimaryCycleEvent(cycle.cycleEvents);
+                data = addDataToRow(data, primaryEvent);
+                let attempt = 0;
+                let scored = 0;
+                cycle.cycleEvents.forEach(event => {
+                    if (event.type === primaryEvent) {
+                        if (event.success) {
+                            attempt += 1;
+                            scored += event.success;
+                        }
+                    }
+                });
+                data = addDataToRow(data, attempt);
+                data = addDataToRow(data, scored);
+                data = addDataToRow(data, 'defended???');
+                data = addDataToRow(data, 'and then?');
             }
         });
+        allData = allData + data + '\n';
     });
-    return '';
+    return allData;
 };
 
 const getPhaseFromStartTime = (start: number): Phase => {
-        if (gameProperties.endgame.start && gameProperties.teleop.start) {
-            if (start < gameProperties.endgame.start) {
-                return 'ENDGAME';
-            } else if (start < gameProperties.teleop.start) {
-                return 'TELEOP';
-            } else {
-                return 'AUTO';
-            }
+    if (gameProperties.endgame.start && gameProperties.teleop.start) {
+        if (start < gameProperties.endgame.start) {
+            return 'ENDGAME';
+        } else if (start < gameProperties.teleop.start) {
+            return 'TELEOP';
+        } else {
+            return 'AUTO';
         }
-        return 'AUTO';
     }
+    return 'AUTO';
+};
+
+const addEmptyColumns = (row: string, columns: number): string => {
+    for (let i = 0; i < columns; i++) {
+        row = addDataToRow(row, '');
+    }
+    return row;
 };
 
 const addDataToRow = (row: string, col: string | number): string => {
-    return `${row},${col}`;
+    if (row !== '') {
+        return `${row},${col}`;
+    }
+    return col.toString();
 };
 
 const getPrimaryCycleEvent = (events: Array<IRobotEvent>): string => {
