94fffc3ec61cd8f41a868530dc3be5d57c137ce6
NicolasNewman <nanewman01@gmail.com>
2020-02-12 11:11:58 -0500

    began updating data redux manager

--- client/src/actions/data.ts
+++ client/src/actions/data.ts
@@ -1,5 +1,5 @@
 import { compileData } from "../helper/matchDataCompiler";
-import { ICompetitionData } from "../reducers/data";
+import { DataState } from "../reducers/data";
 
 export enum DataTypeKeys {
   UPDATE_MATCH_DATA = "UPDATE_MATCH_DATA"
@@ -7,7 +7,7 @@ export enum DataTypeKeys {
 
 interface UpdateMatchDataAction {
   type: DataTypeKeys.UPDATE_MATCH_DATA;
-  data: ICompetitionData;
+  data: DataState;
 }
 
 export type DataTypes = UpdateMatchDataAction;
@@ -15,7 +15,7 @@ export type DataTypes = UpdateMatchDataAction;
 /**
  * Updates the match data stored in the state. This is generally called after all the scouts have submitted their forms
  */
-export function updateMatchData(data: ICompetitionData) {
+export function updateMatchData(data: DataState) {
   return {
     type: DataTypeKeys.UPDATE_MATCH_DATA,
     data
@@ -25,7 +25,7 @@ export function updateMatchData(data: ICompetitionData) {
 // TODO proper TS integration
 export function setMatchData() {
   return (dispatch: any) => {
-    compileData().then(data => {
+    compileData().then((data: any) => {
       dispatch(updateMatchData(data));
     });
   };
